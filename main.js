fd765283a6383afbef5a72370c901557daa2445a
yuval weiss <yuvalweiss1403@gmail.com>
2023-01-04 12:00:32 +0200

    Dom

---
+++ main.js
@@ -0,0 +1,52 @@
+//--------------------------------------------------------------------------------------------
+// function changeH2(e){
+//     e.preventDefault();
+//     let h2 = document.querySelector('h2');
+//     h2.textContent = document.getElementById("input").value;
+//     document.getElementById("input").value =" ";
+//     h2.className = "spin";
+// }
+
+//------------------------------------------------------------------------------------------
+// let counterEx2_2 =0; 
+
+// function counter(e){
+//     counterEx2_2++;
+//     let p = document.getElementById("count");
+//     p.textContent =counterEx2_2;
+//     p.style.fontSize = `${counterEx2_2*10}px`;
+// }
+//-----------------------------------------------------------------------------------
+// window.onload = changeText();
+
+// function changeText(){
+//     let h1 = document.getElementById('heading');
+//     h1.textContent = "Hey "+ (Math.floor(Math.random()*100));
+// }
+//--------------------------------------------------------------------------------------------------
+
+// let submit = document.getElementById('submitButton');
+// submit.addEventListener("click",checkSubmission());
+
+// function checkSubmission(){
+//     let name = document.getElementById('name').value;
+//     let email = document.getElementById('email').value;
+//     if(name =="" || email == ""){
+//         alert("invalid details")
+//     }
+//         name = "";
+//         email = "";
+// }
+//--------------------------------------------------------------------------------------------
+
+// let helloButton = document.getElementById("hello");
+// let goodbyeButton = document.getElementById("goodbye");
+
+// helloButton.addEventListener('click', () => {
+//     console.log('hello');
+//   });
+  
+//   goodbyeButton.addEventListener('click', () => {
+//     console.log('goodbye');
+//   });
+//------------------------------------------------------------------------------------------------
